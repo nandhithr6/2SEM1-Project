94d7355e663c5011e7f31bfbfb75a13e9f4a726a
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2024-08-16 19:48:11 +0530

    Sample_working_prototype-1

--- recipe_app/recipes/models.py
+++ recipe_app/recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='recipes/')
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    is_inspiring = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Update(models.Model):
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
--- recipe_app/recipes/views.py
+++ recipe_app/recipes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+def home(request):
+    # Fetch inspiring recipes - adjust query as needed
+    inspiring_recipes = Recipe.objects.all()[:6]  # Example: get the first 6 recipes
+    context = {
+        'inspiring_recipes': inspiring_recipes,
+    }
+    return render(request, 'home.html', context)
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    context = {
+        'recipe': recipe,
+    }
+    return render(request, 'recipe_detail.html', context)
